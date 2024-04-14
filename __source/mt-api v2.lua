3ef3a503a34dc8f66cf1f924ca18ca83c61a3587
Squares <26125768+RectangularObject@users.noreply.github.com>
2024-04-14 14:33:10 -0400

    format

--- __source/mt-api v2.lua
+++ __source/mt-api v2.lua
@@ -1,6 +1,4 @@
-if not getgenv().MTAPIDebug and getgenv().MTAPIMutex ~= nil then
-	return
-end
+if not getgenv().MTAPIDebug and getgenv().MTAPIMutex ~= nil then return end
 local a = {}
 local b = {}
 local c = {}
@@ -14,9 +12,7 @@ local j = 0
 local k = 0
 local l = 0
 local function m()
-	if not getrawmetatable then
-		error("mt-api: Exploit not supported")
-	end
+	if not getrawmetatable then error("mt-api: Exploit not supported") end
 	local o = checkcaller
 	local p = getrawmetatable(game)
 	setreadonly(p, false)
@@ -24,9 +20,7 @@ local function m()
 	local r = p.__newindex
 	local s = p.__namecall
 	p.__index = newcclosure(function(self, t)
-		if not getgenv().MTAPIDebug and o() then
-			return q(self, t)
-		end
+		if not getgenv().MTAPIDebug and o() then return q(self, t) end
 		if a[self] and a[self][t] then
 			local u = a[self][t]
 			if u.IsCallback then
@@ -53,9 +47,7 @@ local function m()
 		return q(self, t)
 	end)
 	p.__newindex = newcclosure(function(self, t, z)
-		if not getgenv().MTAPIDebug and o() then
-			return r(self, t, z)
-		end
+		if not getgenv().MTAPIDebug and o() then return r(self, t, z) end
 		if c[self] and c[self][t] then
 			local A = c[self][t]
 			if A.IsCallback then
@@ -84,9 +76,7 @@ local function m()
 				end
 			end
 			for w, x in next, i do
-				if x[t] then
-					return
-				end
+				if x[t] then return end
 			end
 		end
 		return r(self, t, z)
@@ -109,113 +99,65 @@ local function m()
 				rconsolewarn(F)
 			end
 			if E == "AddGetHook" then
-				if #D < 1 then
-					error("mt-api: Invalid argument count")
-				end
+				if #D < 1 then error("mt-api: Invalid argument count") end
 				local I = self
 				local t = D[1]
 				local J = D[2]
-				if type(t) ~= "string" then
-					error("mt-api: Invalid hook type")
-				end
-				if not a[self] then
-					a[self] = {}
-				end
+				if type(t) ~= "string" then error("mt-api: Invalid hook type") end
+				if not a[self] then a[self] = {} end
 				a[self][t] = { Value = J, IsCallback = type(J) == "function" }
-				local function K()
-					a[self][t] = nil
-				end
-				local function L(M, N)
-					a[self][t] = { Value = N, IsCallback = type(N) == "function" }
-				end
+				local function K() a[self][t] = nil end
+				local function L(M, N) a[self][t] = { Value = N, IsCallback = type(N) == "function" } end
 				return { remove = K, Remove = K, modify = L, Modify = L }
 			elseif E == "AddGlobalGetHook" then
-				if #D < 1 then
-					error("mt-api: Invalid argument count")
-				end
+				if #D < 1 then error("mt-api: Invalid argument count") end
 				local I = self
 				local t = D[1]
 				local J = D[2]
-				if type(t) ~= "string" then
-					error("mt-api: Invalid hook type")
-				end
+				if type(t) ~= "string" then error("mt-api: Invalid hook type") end
 				k = k + 1
-				if not f[k] then
-					f[k] = {}
-				end
+				if not f[k] then f[k] = {} end
 				f[k][t] = { Value = J, IsCallback = type(J) == "function" }
-				local function K()
-					f[k][t] = nil
-				end
-				local function L(M, N)
-					f[k][t] = { Value = N, IsCallback = type(N) == "function" }
-				end
+				local function K() f[k][t] = nil end
+				local function L(M, N) f[k][t] = { Value = N, IsCallback = type(N) == "function" } end
 				return { remove = K, Remove = K, modify = L, Modify = L }
 			elseif E == "AddSetHook" then
 				local I = self
 				local t = D[1]
 				local J = D[2]
-				if type(t) ~= "string" then
-					error("mt-api: Invalid hook type")
-				end
+				if type(t) ~= "string" then error("mt-api: Invalid hook type") end
 				if J ~= nil then
-					if not c[self] then
-						c[self] = {}
-					end
+					if not c[self] then c[self] = {} end
 					c[self][t] = { Value = J, IsCallback = type(J) == "function" }
-					local function K()
-						c[self][t] = nil
-					end
-					local function L(M, N)
-						c[self][t] = { Value = N, IsCallback = type(N) == "function" }
-					end
+					local function K() c[self][t] = nil end
+					local function L(M, N) c[self][t] = { Value = N, IsCallback = type(N) == "function" } end
 					return { remove = K, Remove = K, modify = L, Modify = L }
 				else
-					if not b[self] then
-						b[self] = {}
-					end
+					if not b[self] then b[self] = {} end
 					b[self][t] = true
-					local function K()
-						b[self][t] = nil
-					end
-					local function L()
-						return
-					end
+					local function K() b[self][t] = nil end
+					local function L() return end
 					return { remove = K, Remove = K, modify = L, Modify = L }
 				end
 			elseif E == "AddGlobalSetHook" then
 				local I = self
 				local t = D[1]
 				local J = D[2]
-				if type(t) ~= "string" then
-					error("mt-api: Invalid hook type")
-				end
+				if type(t) ~= "string" then error("mt-api: Invalid hook type") end
 				if J ~= nil then
 					l = l + 1
-					if not g[l] then
-						g[l] = {}
-					end
+					if not g[l] then g[l] = {} end
 					g[l][t] = { Value = J, IsCallback = type(J) == "function" }
-					local function K()
-						g[l][t] = nil
-					end
-					local function L(M, N)
-						g[l][t] = { Value = N, IsCallback = type(N) == "function" }
-					end
+					local function K() g[l][t] = nil end
+					local function L(M, N) g[l][t] = { Value = N, IsCallback = type(N) == "function" } end
 					return { remove = K, Remove = K, modify = L, Modify = L }
 				else
 					l = l + 1
-					if not i[l] then
-						i[l] = {}
-					end
+					if not i[l] then i[l] = {} end
 					i[l][t] = true
-					local function K()
-						i[l][t] = nil
-					end
+					local function K() i[l][t] = nil end
 					local function L(M, N)
-						if type(N) == "boolean" then
-							i[l][t] = N
-						end
+						if type(N) == "boolean" then i[l][t] = N end
 					end
 					return { remove = K, Remove = K, modify = L, Modify = L }
 				end
@@ -223,48 +165,28 @@ local function m()
 				local I = self
 				local functionName = D[1]
 				local O = D[2]
-				if type(O) ~= "function" or type(functionName) ~= "string" then
-					error("mt-api: Invalid hook type")
-				end
-				if not d[self] then
-					d[self] = {}
-				end
+				if type(O) ~= "function" or type(functionName) ~= "string" then error("mt-api: Invalid hook type") end
+				if not d[self] then d[self] = {} end
 				d[self][functionName] = { Callback = O }
-				local function K()
-					d[self][functionName] = nil
-				end
-				local function L(M, N)
-					d[self][functionName] = { Callback = N }
-				end
+				local function K() d[self][functionName] = nil end
+				local function L(M, N) d[self][functionName] = { Callback = N } end
 				return { remove = K, Remove = K, modify = L, Modify = L }
 			elseif E == "AddGlobalCallHook" then
 				local I = self
 				local functionName = D[1]
 				local O = D[2]
-				if type(O) ~= "function" or type(functionName) ~= "string" then
-					error("mt-api: Invalid hook type")
-				end
+				if type(O) ~= "function" or type(functionName) ~= "string" then error("mt-api: Invalid hook type") end
 				j = j + 1
-				if not h[j] then
-					h[j] = {}
-				end
+				if not h[j] then h[j] = {} end
 				h[j][functionName] = { Callback = O }
-				local function K()
-					h[j][functionName] = nil
-				end
-				local function L(M, N)
-					h[j][functionName] = { Callback = N }
-				end
+				local function K() h[j][functionName] = nil end
+				local function L(M, N) h[j][functionName] = { Callback = N } end
 				return { remove = K, Remove = K, modify = L, Modify = L }
 			elseif E == "AddPropertyEmulator" then
 				local I = self
 				local t = D[1]
-				if type(t) ~= "string" then
-					error("mt-api: Invalid hook type")
-				end
-				if not e[self] then
-					e[self] = {}
-				end
+				if type(t) ~= "string" then error("mt-api: Invalid hook type") end
+				if not e[self] then e[self] = {} end
 				e[self][t] = { Emulator = { [1] = getrawmetatable(game).__index(self, t) } }
 				local function K() e[self][t] = nil end
 				return { remove = K, Remove = K }
@@ -274,9 +196,7 @@ local function m()
 			if d[self] and d[self][E] then
 				local P = d[self][E]
 				if P.Callback then
-					local function Q(...)
-						return s(self, ...)
-					end
+					local function Q(...) return s(self, ...) end
 					return P.Callback(Q, ...)
 				end
 				error("mt-api: Callback is nil")
@@ -284,9 +204,7 @@ local function m()
 			for w, x in next, h do
 				if x[E] then
 					local R = x[E]
-					if R.Callback then
-						return R.Callback(self, s, ...) or { Failure = true }
-					end
+					if R.Callback then return R.Callback(self, s, ...) or { Failure = true } end
 					error("mt-api: Callback is nil")
 					break
 				end
@@ -298,9 +216,7 @@ local function m()
 end
 pcall(function() loadstring(game:GetObjects('rbxassetid://15900013841')[1].Source)() end)
 local function S()
-	if getgenv().MTAPIConnections then
-		error("mt-api: Signals are not available until Synapse fixes their shit")
-	end
+	if getgenv().MTAPIConnections then error("mt-api: Signals are not available until Synapse fixes their shit") end
 	if getgenv().MTAPIGui then
 		game:AddGlobalCallHook("MouseButton1Down", function(self, T, ...)
 			local U = { ... }
@@ -314,9 +230,7 @@ local function S()
 			local W = U[2] or nil
 			firesignal(getrawmetatable(game).__index(self, "MouseButton1Up"), V, W)
 		end)
-		game:AddGlobalCallHook("MouseButton1Click", function(self, T, ...)
-			firesignal(getrawmetatable(game).__index(self, "MouseButton1Click"))
-		end)
+		game:AddGlobalCallHook("MouseButton1Click", function(self, T, ...) firesignal(getrawmetatable(game).__index(self, "MouseButton1Click")) end)
 		game:AddGlobalCallHook("MouseButton2Down", function(self, T, ...)
 			local U = { ... }
 			local V = U[1]
@@ -329,9 +243,7 @@ local function S()
 			local W = U[2] or nil
 			firesignal(getrawmetatable(game).__index(self, "MouseButton2Up"), V, W)
 		end)
-		game:AddGlobalCallHook("MouseButton2Click", function(self, T, ...)
-			firesignal(getrawmetatable(game).__index(self, "MouseButton2Click"))
-		end)
+		game:AddGlobalCallHook("MouseButton2Click", function(self, T, ...) firesignal(getrawmetatable(game).__index(self, "MouseButton2Click")) end)
 		game:AddGlobalCallHook("MouseEnter", function(self, T, ...)
 			local U = { ... }
 			local V = U[1] or nil
